c282c1b2a967acc6024a0d5127ac67ba5fbec666
Clare Robertson <clare.robertson@gaincredit.com>
2020-04-18 11:29:28 +0100

    Updates

---
+++ precache-manifest.bd2a0edd12f61e8cee95451fc1084baa.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90f4246a573125f9d4c7b9fec3f9f0e8",
+    "url": "/lockdown-2020/index.html"
+  },
+  {
+    "revision": "c89d81e3043a47b4b498",
+    "url": "/lockdown-2020/static/css/main.47202b5f.chunk.css"
+  },
+  {
+    "revision": "3e5bd7c3a00342022dd6",
+    "url": "/lockdown-2020/static/js/2.a81acf76.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lockdown-2020/static/js/2.a81acf76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c89d81e3043a47b4b498",
+    "url": "/lockdown-2020/static/js/main.759e9d3d.chunk.js"
+  },
+  {
+    "revision": "46dee1ac8996dd513df4",
+    "url": "/lockdown-2020/static/js/runtime-main.58404b60.js"
+  },
+  {
+    "revision": "a4bd215eb86780afc29db57bd2f8a3c3",
+    "url": "/lockdown-2020/static/media/book.a4bd215e.gif"
+  },
+  {
+    "revision": "9091ad347261930db45f2d37ab57a68e",
+    "url": "/lockdown-2020/static/media/bunny.9091ad34.gif"
+  },
+  {
+    "revision": "93592f45dc9efc2d37567ba808667939",
+    "url": "/lockdown-2020/static/media/bus.93592f45.gif"
+  },
+  {
+    "revision": "ce639acef2907a21409c6a03f5d94448",
+    "url": "/lockdown-2020/static/media/chocolate.ce639ace.gif"
+  },
+  {
+    "revision": "2359d0f68afcc40cdfb51f63896f81cb",
+    "url": "/lockdown-2020/static/media/date.2359d0f6.gif"
+  },
+  {
+    "revision": "1d7643e7d4b07558146372e25fb24a40",
+    "url": "/lockdown-2020/static/media/laptop.1d7643e7.gif"
+  },
+  {
+    "revision": "34db96cacbe7594a23759a6f98448bd0",
+    "url": "/lockdown-2020/static/media/shame.34db96ca.gif"
+  },
+  {
+    "revision": "3e23427ac5cd34288b67e408f016ac04",
+    "url": "/lockdown-2020/static/media/share.3e23427a.gif"
+  },
+  {
+    "revision": "45139c623f3bb0b16f9cf2058eb618e5",
+    "url": "/lockdown-2020/static/media/shopping.45139c62.gif"
+  },
+  {
+    "revision": "7a5dd52420ebb621d81f723dfe34902b",
+    "url": "/lockdown-2020/static/media/sticks.7a5dd524.gif"
+  },
+  {
+    "revision": "7d1571695ad9cc54940ee3061c221e39",
+    "url": "/lockdown-2020/static/media/tape.7d157169.gif"
+  },
+  {
+    "revision": "29c4fd28f9ab28e70d024242fcb7b160",
+    "url": "/lockdown-2020/static/media/taytos.29c4fd28.gif"
+  },
+  {
+    "revision": "377a1b84791dcc064f4925c0cdd8f6da",
+    "url": "/lockdown-2020/static/media/time.377a1b84.gif"
+  },
+  {
+    "revision": "4e1d53a5d812b893195d92bb740e1b4d",
+    "url": "/lockdown-2020/static/media/tpaper.4e1d53a5.gif"
+  },
+  {
+    "revision": "614e12fab6840c72402813eda696cf0c",
+    "url": "/lockdown-2020/static/media/treatyoself.614e12fa.gif"
+  },
+  {
+    "revision": "1e21385fe7ff12994a26dcf968bfd6e2",
+    "url": "/lockdown-2020/static/media/two.1e21385f.gif"
+  },
+  {
+    "revision": "b0a9a58122562e727646d107fc8a296a",
+    "url": "/lockdown-2020/static/media/twometres.b0a9a581.gif"
+  }
+]);
